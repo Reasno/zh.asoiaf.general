9b8db0131660610757b78d929404c86728482b12
Glen Gu <guxi99@gmail.com>
2014-08-22 09:58:37 +0800

    add fallback

--- category.js
+++ category.js
@@ -23,9 +23,9 @@ var bot = require('nodemw');
 
 var lg = false;
 
-var stash = ['Images of Jaqen H\'ghar','[[Category:贾昆·赫加尔图片]]','Images of Jon Snow‎','[[Category:琼恩·雪诺图片]]','Images of Eddard Stark‎','[[Category:艾德·史塔克图片]]','Images of Daenerys Targaryen‎','[[category:丹妮莉丝·坦格利安图片]]','Images of Catelyn Tully‎','[[Category:凯特琳·徒利图片]]','Images of Bran Stark‎','[[Category:布兰·史塔克图片]]','Images of Arya Stark‎','[[category:艾莉亚·史塔克图片]]'];
+var stash = ['Map_Images','[[Category:地图]]','Images of Gregor Clegane‎','[[Category:格雷果·克里冈图片]]','Coat of arms images','[[Category:纹章图片]]','Images of Eddard Stark‎','[[Category:艾德·史塔克图片]]','Images of Daenerys Targaryen‎','[[category:丹妮莉丝·坦格利安图片]]','Images of Catelyn Tully‎','[[Category:凯特琳·徒利图片]]','Images of Bran Stark‎','[[Category:布兰·史塔克图片]]','Images of Arya Stark‎','[[category:艾莉亚·史塔克图片]]'];
 var category = function(){
-	var self = this;
+	var self = this
 	self.execute = function() {
 		try{
 
@@ -37,6 +37,24 @@ var category = function(){
 				}else{
 					lg = false;
 					return;
+				}
+				try{
+					where_are_my_dragons(zh);
+				}catch(err){
+					try{
+						where_are_my_dragons(zh);
+					}catch(err){
+						return
+					}
+				}
+				try{
+					where_are_my_dragons(zh);
+				}catch(err){
+					try{
+						where_are_my_dragons(zh);
+					}catch(err){
+						return
+					}
 				}
 				try{
 					where_are_my_dragons(zh);
@@ -84,35 +102,43 @@ var category = function(){
 							titles : entity.pages[i].title
 						}
 						//sleep.sleep(2);
-						zh.api.call(params,function(data){	
-							var content = JSON.stringify(data);
-							content = escapeRegExp(content);
-							var my_obj = JSON.parse(JSON.stringify(data));
-							var my_title ='';
-							var my_ns='';
-							var my_content ='';
-							for (var l in my_obj['pages']){
-								for (var m in my_obj['pages'][l]){
-									if (m == 'title'){
-										my_title = my_obj['pages'][l]['title'];
-										my_ns = my_obj['pages'][l]['ns'];
+						try{
+							zh.api.call(params,function(data){	
+								var content = JSON.stringify(data);
+								content = escapeRegExp(content);
+								try{
+									var my_obj = JSON.parse(JSON.stringify(data));
+								}catch(err){
+									return;
+								}
+								var my_title ='';
+								var my_ns='';
+								var my_content ='';
+								for (var l in my_obj['pages']){
+									for (var m in my_obj['pages'][l]){
+										if (m == 'title'){
+											my_title = my_obj['pages'][l]['title'];
+											my_ns = my_obj['pages'][l]['ns'];
 
-										try{
-											my_content = my_obj['pages'][l]['revisions'][0]['*'];
-											break;
-										}catch(err){
-											//this is a portal
-											break;
-										}	
+											try{
+												my_content = my_obj['pages'][l]['revisions'][0]['*'];
+												break;
+											}catch(err){
+												//this is a portal
+												break;
+											}	
+										}
 									}
 								}
-							}
 
-							zh.edit(my_title,my_content+stash[1],'分类',function(data){
-								console.log('category added');
-							})
+								zh.edit(my_title,my_content+stash[1],'分类',function(data){
+									console.log('category added');
+								})
 
-						});
+							});
+						}catch(err){
+							return;
+						}
 
 					}
 				});
